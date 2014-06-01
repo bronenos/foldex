ba0c551509525a90e0edbdaf19d1bcc108242f54
Stan Potemkin <bronenos@gmail.com>
2014-06-01 09:06:42 +0400

    Initial Commit

---
+++ Foldex/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Foldex
+//
+//  Created by Stan Potemkin on 6/1/14.
+//  Copyright (c) 2014 bronenos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
